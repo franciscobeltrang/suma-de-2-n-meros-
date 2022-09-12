813ad9a2fe8a57853b808b1eb510ed54ea4964cd
franciscobeltrang <113156431+franciscobeltrang@users.noreply.github.com>
2022-09-11 22:37:55 -0600

    Add files via upload

---
+++ suma de 2 números.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int numero1;
+   int numero2;
+   
+    cout<<"ingresa numero1"<<endl;
+     cin>>numero1;
+    cout<<"ingresa numero2"<<endl;
+    cin>>numero2;
+    cout<<"la suma de los números es: "<<numero1+numero2;
+    return 0;
+}
